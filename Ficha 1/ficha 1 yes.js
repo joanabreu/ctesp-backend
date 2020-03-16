944ee1732898ce8783899cac1e8481d7856678d0
joanabreu <joanabreu86@hotmail.com>
2020-03-16 22:17:24 +0000

    Ficha 1 feita

--- Ficha 1/ficha 1 yes.js
+++ Ficha 1/ficha 1 yes.js
@@ -168,3 +168,14 @@ function getMin(array){
 var min = getMin(array);
 console.log("Min:" + min);
     
+function getMin(array){
+    var min = array[0];
+    for (let i = 1; i < array.length; i++) {
+        if(array[i] < min){
+            min = array[i];
+        }
+    }
+    return min;
+}
+var min = getMin(array);
+console.log("Min:" + min);
