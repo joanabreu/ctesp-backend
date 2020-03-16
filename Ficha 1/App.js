cf811366dd08153eb01c677ef77d3ce0a65eac4c
joanabreu <joanabreu86@hotmail.com>
2020-03-16 19:29:50 +0000

    ficha 1 alínea 9

--- Ficha 1/App.js
+++ Ficha 1/App.js
@@ -2,7 +2,7 @@ var i =0;
 var d = 0.5;
 var str ="Hello";
 
-function calcularFinalGrade()
+function calcularFinalGrade(practical , theoreticalGrade)
 var finalGrade = (practical + theoreticalGrade) /2;
 return finalGrade;
 }
@@ -39,6 +39,18 @@ function monthByName(monthNumber){
         case 2:
             console.log("February");
             break;
+        case 3:
+            console.log("March");
+            break;
+        case 4:
+            console.log("April");
+            break;
+        case 5:
+            console.log("May");
+            break;
+        case 6:
+            console.log("June");
+            break;
         default:
             console.log("N/A");
             break;
@@ -46,6 +58,42 @@ function monthByName(monthNumber){
 }
 
 monthByName(3);
-monthByName(1);
 /*Evocar a função*/
+
+/*Alínea7*/
+
+function calculator(operand1, operand2, operador){
+    if(operator == '+'){
+        console.log(operand1 + operand2);
+    }
+        
+    else if(operator == '-'){
+        console.log(operand1 - operand2);
+}
+
+    else if(operator == '*'){
+    console.log(operand1 * operand2);
+}
+
+    else if(operator == '/'){
+    console.log(operand1 / operand2);
+}
+
+calculator(2,2, '+');
+
+/*Alínea 8*/
+function multipleOfFive(value){
+    for (let i = 1; i <= value; i++) {
+        if(i%5 == 0){
+            console.log(i);
+        }
+    }        
+}
+
+multipleOfFive(20);
+
+/*Alínea 9*/
+function multipleOfFive(){
+    
+}
 
